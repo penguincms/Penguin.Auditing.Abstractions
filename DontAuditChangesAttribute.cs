c82f61f23321b3a89a051db7f82ec989aa4cd7b6
MrJackSpade <loffredo.ap@gmail.com>
2019-10-05 23:43:50 -0700

    Add files via upload

---
+++ DontAuditChangesAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Penguin.Cms.Auditing.Abstractions
+{
+    /// <summary>
+    /// Informs the auditing system not to track changes on a property or class
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property, AllowMultiple =false, Inherited = true)]
+    public class DontAuditChangesAttribute : Attribute
+    {
+    }
+}
