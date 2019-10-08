e6c55e11f424b0bbf600a7da979482d7d5906fae
anloffredo <anloffredo@massageenvy.com>
2019-10-08 08:14:12 -0700

    Framework Updates
    Framework Updates

---
+++ Attributes/DontAuditChangesAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Penguin.Auditing.Abstractions.Attributes
+{
+
+    /// <summary>
+    /// Informs an entity auditor not to track the property or class
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class DontAuditChangesAttribute : Attribute
+    {
+    }
+}
